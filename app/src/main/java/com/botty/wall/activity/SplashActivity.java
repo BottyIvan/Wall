dec0ec67cffeeefc914dc3a7eb9ef4487793bea4
BottyIvan <testbottyivan@gmail.com>
2016-08-17 20:38:24 +0200

    Intro

--- app/src/main/java/com/botty/wall/activity/SplashActivity.java
+++ app/src/main/java/com/botty/wall/activity/SplashActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 100;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
